5adf7ce903d6e2840400eee0645563366969dc8c
diptapaul <diptapaul000@gmail.com>
2018-04-02 01:48:12 +0600

    syntax added

---
+++ syntax.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Syntax</title>
+	<style type="text/css">
+		.phpcoding{
+			width: 900px;
+			margin: 0 auto;
+			background: #ddd;
+			min-height: 400px;
+		}
+		.headeroption, .footeroption{
+			background: #444;
+			color: #fff;
+			text-align: center;
+			padding: 20px;
+		}
+		.maincontent{
+			min-height: 400px;
+			padding: 20px;
+		}
+		.headeroption h2, .footeroption h2{
+			margin: 0;
+		}
+	</style>
+</head>
+ 
+<body>
+<div class = "phpcoding">
+	<section class = "headeroption">
+		<h2>PHP Fundamentals Training</h2>
+	</section>
+	<section class = "maincontent">
+	<?php
+		echo "Hello World";
+	?>
+	</section>
+	<section class = "footeroption">
+		<h2>www.trainingwithliveproject.com</h2>
+	</section>
+</div>
+</body>
+</html>
